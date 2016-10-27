04ba83ef20fd051dcdf0579247ada429b1382951
Anna <Anna@Annas-MacBook-Air-2.local>
2016-10-27 14:57:24 -0500

    sql db

--- routes/time.js
+++ routes/time.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var distance = require('google-distance');
+var connectionString = 'postgres://localhost:5432/times';
+var path = require('path');
+var pg = require('pg');
+
 console.log( 'inside routes/time.js');
 
-var data = 0;
-var distance = require('google-distance');
-distance.apiKey = "AIzaSyAvbUTpswKFmmzVTkbqbXS1H-gjvtviG_U";
+var timeLogged = {
+  origin: '',
+  destination: '',
+  mode: '',
+  time: ''
+};
 
+distance.apiKey = "AIzaSyAvbUTpswKFmmzVTkbqbXS1H-gjvtviG_U";
 // var timerFunction = function(){
   // console.log( 'inside timer function');
   distance.get(
@@ -16,33 +25,64 @@ distance.apiKey = "AIzaSyAvbUTpswKFmmzVTkbqbXS1H-gjvtviG_U";
     mode: 'driving'
   },
 
+
 function(err, data) {
     if (err) return console.log(err);
-    console.log(data.duration);
+    console.log(data);
     var i = 1;
     var timer = setTimeout(function(  ) {
       console.log( 'inside timer, data: ' + data.duration );
         i++;
-        timer = setTimeout(arguments.callee, 300000);
-      }, 300000);
+        timer = setTimeout(arguments.callee, 5000);
+      }, 5000);
+      timeLogged = {
+        origin: data.origin,
+        destination: data.destination,
+        mode: data.mode,
+        time: data.duration
+      };
+      console.log( 'timeLogged.origin: ' + timeLogged.origin + ', ' + timeLogged.destination + ', ' + timeLogged.mode + ', ' + timeLogged.time );
+     }
 
-});
-// };
+
+);//end of distance.get
+//
+app.get('/addTime', function( req, res, next ){
+//   console.log( 'inside router.post for reviews', req.body );
+    var timeSaved = {
+      origin: req.body.origin,
+      destination: req.body.destination,
+      mode: req.body.mode,
+      time: req.body.time,
+    };
+    console.log('new review:', timeSaved);
+
+    pg.connect(connectionString, function(err, client, done) {
+      var newTime = client.query("INSERT INTO trip ( origin, destination, mode, time ) VALUES ( $1, $2, $3, $4 ) RETURNING id",
+        [ newTime.origin, newTime.destination, newTime.mode, newTime.time ],
+          function (err, result) {
+            if(err) {
+              console.log("Error inserting data: ", err);
+              next(err);
+            } else {
+              console.log( 'req.body.id: ', req.body.id);
+            }
+            // console.log( 'result: ', result );
+            //   reviewSaved.id = result.rows[0].id;
+            //   reviewSaved.reviewerID = req.user.id;
+              res.send( newTime );
+               done();
+            // client.end();
+
+              // res.redirect('/');
+             //end of else
+          });//end of client.query
+        });//end of pg.connect
+      });//end of router.add post
 
 
 
-
-console.log( 'data: ' + data );
-// console.log( 'time: ' + data.duration );
-// router.post('/', function ( origins, destinations ){
-//   googleMapsClient.geocode({
-//       address: origins,
-//     }, function(err, response){
-//       if(!err) {
-//         console.log( response.json.results);
-//         var originAddress = response.json.results;
-//       }
-//     });
-// });
+console.log( 'timeLogged: ' + timeLogged );
+console.log( 'hello');
 
 module.exports = router;
